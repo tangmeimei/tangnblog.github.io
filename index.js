47b09428e59808ef38e9d54c2c73ae297ae6a02d
tangmeizhi <tangmeizhi@tobosoft.com.cn>
2021-06-16 20:06:26 +0800

    修改

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function showNavMenu(e) {
-    const event = e || window.Event
-    console.log(event)
-    const parent = event.target.parentElement;
-    const ulMenu = parent.querySelector('ul');
-    if (ulMenu.classList.contains('active')) {
-        ulMenu.classList.remove('active');
-    } else {
-        ulMenu.classList.add('active');
+
+let nav = (function () {
+
+    function showNavMenu(e) {
+        const event = e || window.Event
+        const parent = event.target.parentElement;
+        const ulMenu = parent.querySelector('ul');
+        if (ulMenu.classList.contains('active')) {
+            ulMenu.classList.remove('active');
+        } else {
+            ulMenu.classList.add('active');
+        }
     }
+
+    return {
+        showNavMenu
+    }
+
+}(window));
+
+
+window.onload = function () {
+
+
+}
+
+window.onresize = function () {
+
 }
